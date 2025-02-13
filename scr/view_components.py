f5b0558f5d15b3d77190cdd248d8dcd0dfbbe0c8
Nemex81 Luca <nemex1981@gmail.com>
2025-02-13 01:22:38 +0100

    add models for view aggiunta di classi modello per la gestione delle finestre di dialogo e dei componenti dell'interfaccia utente

--- scr/view_components.py
+++ scr/view_components.py
@@ -1,29 +1,28 @@
 """
-    Modulo per la gestione dei componenti delle views
+        Modulo per la gestione dei componenti delle views
 
-    path:
-        ./scr/view_components.py
+        path:
+            ./scr/view_components.py
 
     Descrizione:
-    
-        
+        Questo modulo contiene classi base per la creazione di finestre di dialogo e componenti dell'interfaccia utente.
+        La classe BasicView fornisce un'interfaccia comune per le finestre di dialogo, mentre le classi derivate come 
+        SingleCardView e ListView forniscono funzionalità specifiche per la gestione di carte e elenchi.
 
 """
 
-#lib
-import os, re, shutil, pyperclip, wx
-from .db import session, db_session, Deck, DeckCard, Card
+# Lib
+import wx
 from abc import ABC, abstractmethod
-from utyls import enu_glob as eg
-from utyls import logger as log
-#import pdb
-
+from utyls import helper as hp
+from utyls.enu_glob import EnuCardType, EnuSpellSubType, EnuPetSubType, EnuRarity, EnuExpansion, EnuSpellType
 
 
 class BasicView(wx.Dialog):
     """
         Classe base per le finestre di dialogo dell'interfaccia utente.
     """
+
     def __init__(self, parent, title, size=(500, 400)):
         super().__init__(parent=parent, title=title, size=size)
         self.parent = parent
@@ -32,8 +31,8 @@ class BasicView(wx.Dialog):
         self.Show()
 
     def init_ui(self):
-        """ Inizializza l'interfaccia utente. """
-        self.SetBackgroundColour('black')
+        """Inizializza l'interfaccia utente con le impostazioni comuni a tutte le finestre."""
+        self.SetBackgroundColour('blue')
         self.panel = wx.Panel(self)
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.panel.SetSizer(self.sizer)
@@ -41,15 +40,117 @@ class BasicView(wx.Dialog):
 
     @abstractmethod
     def init_ui_elements(self, *args, **kwargs):
-        """ Inizializza gli elementi dell'interfaccia utente. """
+        """Inizializza gli elementi dell'interfaccia utente."""
         pass
 
     def close(self, event):
-        """ Chiude la finestra di dialogo. """
-        self.close()
+        """Chiude la finestra di dialogo."""
+        self.EndModal(wx.ID_CANCEL)
 
 
 
+class SingleCardView(BasicView):
+    """
+    Classe base per finestre che gestiscono i campi di una singola carta.
+    Utilizzata per finestre come "Aggiungi Carta", "Modifica Carta" o "Filtri".
+    """
+
+    def __init__(self, parent, title, size=(400, 500)):
+        super().__init__(parent, title, size)
+        self.card_data = {}  # Dizionario per memorizzare i dati della carta
+
+    def init_ui_elements(self):
+        """Inizializza i campi comuni per una singola carta."""
+        self.fields = [
+            ("nome", wx.TextCtrl),
+            ("costo_mana", wx.SpinCtrl, {"min": 0, "max": 20}),
+            ("tipo", wx.ComboBox, {"choices": [t.value for t in EnuCardType], "style": wx.CB_READONLY}),
+            ("tipo_magia", wx.ComboBox, {"choices": [t.value for t in EnuSpellType], "style": wx.CB_READONLY}),
+            ("sottotipo", wx.ComboBox, {"choices": [], "style": wx.CB_READONLY}),
+            ("attacco", wx.SpinCtrl, {"min": 0, "max": 20}),
+            ("vita", wx.SpinCtrl, {"min": 0, "max": 20}),
+            ("durability", wx.SpinCtrl, {"min": 0, "max": 20}),
+            ("rarita", wx.ComboBox, {"choices": [r.value for r in EnuRarity], "style": wx.CB_READONLY}),
+            ("espansione", wx.ComboBox, {"choices": [e.value for e in EnuExpansion], "style": wx.CB_READONLY})
+        ]
+
+        self.sizer, self.control_dict = hp.create_ui_controls(self.panel, self.fields)
+
+        # Collega l'evento di selezione del tipo di carta al metodo update_subtypes
+        self.control_dict["tipo"].Bind(wx.EVT_COMBOBOX, self.on_type_change)
+
+    def on_type_change(self, event):
+        """Aggiorna i sottotipi in base al tipo di carta selezionato."""
+        card_type = self.control_dict["tipo"].GetValue()
+        if card_type == EnuCardType.MAGIA.value:
+            subtypes = [st.value for st in EnuSpellSubType]
+        elif card_type == EnuCardType.CREATURA.value:
+            subtypes = [st.value for st in EnuPetSubType]
+        else:
+            subtypes = []
+
+        # Aggiorna i sottotipi
+        self.control_dict["sottotipo"].Clear()
+        self.control_dict["sottotipo"].AppendItems(subtypes)
+
+    def get_card_data(self):
+        """Restituisce i dati della carta inseriti dall'utente."""
+        return {
+            "name": self.control_dict["nome"].GetValue(),
+            "mana_cost": self.control_dict["costo_mana"].GetValue(),
+            "card_type": self.control_dict["tipo"].GetValue(),
+            "spell_type": self.control_dict["tipo_magia"].GetValue(),
+            "card_subtype": self.control_dict["sottotipo"].GetValue(),
+            "attack": self.control_dict["attacco"].GetValue(),
+            "health": self.control_dict["vita"].GetValue(),
+            "durability": self.control_dict["durability"].GetValue(),
+            "rarity": self.control_dict["rarita"].GetValue(),
+            "expansion": self.control_dict["espansione"].GetValue()
+        }
+
+
+
+class ListView(BasicView):
+    """
+        Classe base per finestre che gestiscono elenchi (carte, mazzi, ecc.).
+        Utilizzata per finestre come "Collezione Carte", "Gestione Mazzi" o "Visualizza Mazzo".
+    """
+
+    def __init__(self, parent, title, size=(800, 600)):
+        super().__init__(parent, title, size)
+        self.list_ctrl = None  # ListCtrl per visualizzare l'elenco
+        self.data = []  # Lista dei dati da visualizzare
+
+    def init_ui_elements(self):
+        """Inizializza la lista e i pulsanti comuni."""
+        self.list_ctrl = wx.ListCtrl(
+            self.panel,
+            style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.BORDER_SUNKEN
+        )
+        self.sizer.Add(self.list_ctrl, proportion=1, flag=wx.EXPAND | wx.ALL, border=10)
+
+        # Pulsanti comuni
+        btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        for label in ["Aggiorna", "Chiudi"]:
+            btn = wx.Button(self.panel, label=label)
+            btn_sizer.Add(btn, flag=wx.RIGHT, border=5)
+            if label == "Aggiorna":
+                btn.Bind(wx.EVT_BUTTON, self.on_refresh)
+            elif label == "Chiudi":
+                btn.Bind(wx.EVT_BUTTON, self.on_close)
+        self.sizer.Add(btn_sizer, flag=wx.ALIGN_RIGHT | wx.ALL, border=10)
+
+    def load_data(self):
+        """Carica i dati nell'elenco (da implementare nelle classi derivate)."""
+        raise NotImplementedError("Il metodo load_data deve essere implementato nelle classi derivate.")
+
+    def on_refresh(self, event):
+        """Aggiorna l'elenco."""
+        self.load_data()
+
+    def on_close(self, event):
+        """Chiude la finestra."""
+        self.EndModal(wx.ID_CANCEL)
 
 
 
