5cc2d65ef5142ea4d6519fac130b81226c6cbcd5
Nemex81 Luca <nemex1981@gmail.com>
2025-02-21 01:09:48 +0100

    rimosso codice commentato in deck_view.py

--- scr/views/deck_view.py
+++ scr/views/deck_view.py
@@ -16,10 +16,10 @@
 import wx, pyperclip
 from sqlalchemy.exc import SQLAlchemyError
 from ..db import session, Card, DeckCard, Deck
-from ..models import load_deck_from_db, load_cards
+#from ..models import load_deck_from_db, load_cards
 from .proto_views import BasicView
 from .card_edit_dialog import CardEditDialog
-from utyls.enu_glob import EnuColors, ENUCARD, EnuExtraCard, EnuCardType, EnuSpellType, EnuSpellSubType, EnuPetSubType, EnuHero, EnuRarity, EnuExpansion
+#from utyls.enu_glob import EnuColors, ENUCARD, EnuExtraCard, EnuCardType, EnuSpellType, EnuSpellSubType, EnuPetSubType, EnuHero, EnuRarity, EnuExpansion
 from utyls import helper as hp
 from utyls import logger as log
 
